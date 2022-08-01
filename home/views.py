81f199ee844d8c72ffdf2c267c2003514e15fdd5
Nguyễn Nhật <ng.nhat103@gmail.com>
2022-08-01 09:46:30 +0700

    hello word

--- home/views.py
+++ home/views.py
@@ -1,8 +1,16 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+import datetime
 
 # Create your views here.
+TEMPLATES_DIRS = (
+    'os.path.join(BASE_DIR, "templates"),'
+)
 
 
 def index(request):
-    return HttpResponse("Hello word. You're at home")
+    # return HttpResponse("Hello word. You're at home")
+    # return HttpResponse("<html><head></head><body>.....")
+    today = datetime.datetime.now().date()
+    # return render(request, "index.html")
+    return render(request, "index.html", {"today": today})
--- mysite/urls.py
+++ mysite/urls.py
@@ -19,5 +19,6 @@ from django.conf.urls import include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('home/', include('home.urls')),
+    path('', include('home.urls')),
+    #path('home/', include('home.urls')),
 ]
